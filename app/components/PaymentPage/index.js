303661357da887da92308a2fb2ede8f13559b3ea
Irene <zirenely@gmail.com>
2017-07-08 08:22:12 +0700

    initiate payment page and add photo assets

---
+++ app/components/PaymentPage/index.js
@@ -0,0 +1,24 @@
+/**
+*
+* PaymentPage
+*
+*/
+
+import React from 'react';
+// import styled from 'styled-components';
+
+
+class PaymentPage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+PaymentPage.propTypes = {
+
+};
+
+export default PaymentPage;
